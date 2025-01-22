6b225941e99d3caabaac1b01884905908c4a86bd
Rohit kumar <rohit.kumar@taikishaindia.com>
2025-01-22 10:45:02 +0530

    initial comit

---
+++ manage.py
@@ -0,0 +1,15 @@
+import typer
+from cli.commands.basic import *
+
+@app.callback(invoke_without_command=True)
+def main(ctx: typer.Context):
+    """Main entry point."""
+    if ctx.invoked_subcommand is None:
+        typer.echo(ctx.get_help())
+        raise typer.Exit()
+
+
+if __name__ == "__main__":
+    app()
+
+    
