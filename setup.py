0405ebf5fbfdfb862d78a1b745f7be612298169f
Your Name <you@example.com>
2025-01-28 20:26:22 +0530

    all done

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="fpcli",
-    version="0.1.14",
+    version="0.1.16",
     description="This the simple command line interface using this package  you can easly integrate the command line interface for fastapi which is developed into the typer package ",
     author="Rohit kumar",
     packages=find_packages(include=["fpcli", "fpcli.*"]),
