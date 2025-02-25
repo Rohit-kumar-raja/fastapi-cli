3c0edb37ebd59cbc18c5a47b5c9f968a3dc45c7c
Rohit kumar <rohit.kumar@taikishaindia.com>
2025-02-25 11:52:42 +0530

    bug: fixed the migrations issues

--- fpcli/commands/dbseed.py
+++ fpcli/commands/dbseed.py
@@ -1,8 +1,7 @@
 from pathlib import Path
+import subprocess
 import typer
-import os
-import sys
-import importlib.util
+
 from fpcli.function import check_app
 from .basic import app
 from ..fpcli_settings import APP_FOLDER
@@ -28,29 +27,6 @@ def dbseed(
         )
         raise typer.Exit(1)
 
-    module_name = f"__init__.py"  # Construct module name
-
-    typer.echo(
-        typer.style(f"🌱 Running {module_name}...", typer.colors.BLUE, bold=True)
-    )
-
-    # try:
-    spec = importlib.util.spec_from_file_location(
-        module_name, SEEDER_FOLDER / "__init__.py"
-    )
-    print(spec)
-    module = importlib.util.module_from_spec(spec)
-    sys.modules[module_name] = module
-    spec.loader.exec_module(module)
-
-    # except ModuleNotFoundError as e:
-    #     typer.echo(typer.style(f"❌ Import error: {e}", typer.colors.RED, bold=True))
-    #     typer.echo(
-    #         typer.style(
-    #             f"⚠️  Check if your seeder files use absolute imports!",
-    #             typer.colors.YELLOW,
-    #             bold=True,
-    #         )
-    #     )
+    subprocess.run(["python","-m", f"{APP_FOLDER.replace('/', '.')}{app_name}.seeders"])
 
     typer.echo(typer.style("🎉 Seeding complete!", typer.colors.GREEN, bold=True))
